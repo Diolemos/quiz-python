c3b00e5793d1a136a64ae1d7e45a001f00f7c33e
Dio Lemos <pedro.devsc@outlook.com>
2023-05-15 09:22:09 -0300

    save project to github

---
+++ main.py
@@ -0,0 +1,21 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+import random
+
+# new_question = Question(random.choice(data.question_data))
+question_bank = []
+
+for question in question_data  :
+    text = question['question']
+    answer = question['correct_answer']
+    new_question = Question(text, answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+
+    quiz.next_question()
+print("You've completed the Quiz!")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")    
